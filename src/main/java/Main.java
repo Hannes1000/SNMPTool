cf488f23bd21974a4f4cfe1d0b0eff662da47327
Hannes1000 <60342304+Hannes1000@users.noreply.github.com>
2020-12-18 15:22:07 +0100

    Added: other OIDs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -44,30 +44,88 @@ public class Main {
                     output = "Program shutdown\n";
                     break;
                 case Commands.HELP:
-                    output = "All Working Commands:\n" +
-                            Commands.GetSysDesc + " [IP-Address]" + "\n" +
-                            Commands.GETSYSOBJECTID + " [IP-Address]" + "\n" +
-                            Commands.GETSYSUPTIME + " [IP-Address]" + "\n" +
-                            Commands.EXIT + "\n\n" +
+                    output = "All Working Commands (default ip: " + strIPAddress +"):\n" +
+                            Commands.GetSysDesc + " [IP-Address]" + "\t//Description of the Device\n" +
+                            Commands.GETSYSOBJECTID + " [IP-Address]" + "\t//Oject-id that describes the Device\n" +
+                            Commands.GETSYSUPTIME + " [IP-Address]" + "\t//Time that the Device is running\n" +
+                            Commands.GETSYSCONTACT + " [IP-Address]" + "\t//Contact Person that of the Device\n" +
+                            Commands.GETSYSNAME + " [IP-Address]" + "\t//Name of the Device\n" +
+                            Commands.GETSYSLOCATION + " [IP-Address]" + "\t\t//Location of the Device (not always accurate because user can set it)\n" +
+                            Commands.GETSYSSERVICES + " [IP-Address]" + "\t//How many services are currently running on the Device\n" +
+                            Commands.IPADDRESS + " [IP-Address]" + "\t\t//Change the default IP-Address\n" +
+                            Commands.EXIT + "\t\t\t//Shut down\n\n" +
                             "Other Commands (Not Working Yet):\n" +
                             Commands.SETSNMP + "\n";
                     break;
+                case Commands.IPADDRESS:
+                    if(input.length == 2) {
+                        output = "Changed " + strIPAddress + " to " + input[1];
+                        strIPAddress = input[1];
+                    }else {
+                        output = "Please enter also an IP-Adress";
+                    }
+                    break;
                 case Commands.GetSysDesc:
+                    output = "Device Information: ";
                     if(input.length != 2){
-                        output = "Please enter also the IP-Address after the Command.\n";
+                        output = snmpController.snmpGet(strIPAddress, READ_COMMUNITY, oid);
                     }else {
                         output = snmpController.snmpGet(input[1], READ_COMMUNITY, oid);
                     }
                     break;
                 case Commands.GETSYSOBJECTID:
-                    output = "System Object ID: ";
-                    String objid = snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.2.0");
-                    output += objid;
-                    output += "\nSystem: " + snmpController.snmpGet(input[1], READ_COMMUNITY, objid);
+                    if(input.length != 2){
+                        output = snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.2.0");
+                    }else {
+                        output = snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.2.0");
+                    }
                     break;
                 case Commands.GETSYSUPTIME:
-                    output = snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.3.0");
+                    output = "Device is running for: ";
+                    if(input.length != 2){
+                        output += snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.3.0");
+                    }else {
+                        output += snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.3.0");
+                    }
                     break;
+                case Commands.GETSYSCONTACT:
+                    output = "Device Contact: ";
+                    if(input.length != 2){
+                        output += snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.4.0");
+                    }else {
+                        output += snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.4.0");
+                    }
+                    break;
+                case Commands.GETSYSNAME:
+                    output = "Device Name: ";
+                    if(input.length != 2){
+                        output += snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.5.0");
+                    }else {
+                        output += snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.5.0");
+                    }
+                    break;
+                case Commands.GETSYSLOCATION:
+                    output = "Device Location: ";
+                    if(input.length != 2){
+                        output += snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.6.0");
+                    }else {
+                        output += snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.6.0");
+                    }
+                    break;
+                case Commands.GETSYSSERVICES:
+                    output = "Currently Running Services: ";
+                    if(input.length != 2){
+                        output += snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.7.0");
+                    }else {
+                        output += snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.7.0");
+                    }
+                    break;
+                case "test":
+                    if(input.length != 2){
+                        output = snmpController.snmpGet(strIPAddress, READ_COMMUNITY, "1.3.6.1.2.1.1.4.0");
+                    }else {
+                        output = snmpController.snmpGet(input[1], READ_COMMUNITY, "1.3.6.1.2.1.1.4.0");
+                    };
                 default:
                     output = "Unknown Command:\nType [help] to see all Commands.\n";
                     break;
@@ -77,12 +135,17 @@ public class Main {
     }
 
 
-    private static class Commands {
+    public static class Commands {
         public static final String HELP = "help";
         public static final String EXIT = "exit";
+        public static final String IPADDRESS = "setip";
         public static final String GetSysDesc = "sysdesc";
         public static final String GETSYSOBJECTID = "sysobjid";
         public static final String GETSYSUPTIME = "sysuptime";
+        public static final String GETSYSCONTACT = "syscont";
+        public static final String GETSYSNAME = "sysname";
+        public static final String GETSYSLOCATION = "sysloc";
+        public static final String GETSYSSERVICES = "sysserv";
         public static final String SETSNMP = "setsnmp";
     }
 
